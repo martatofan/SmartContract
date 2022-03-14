4bafbcd9d3ec697c3d036adb2ac180edaa7eed39
martatofan <77557779+martatofan@users.noreply.github.com>
2022-03-14 15:53:32 -0400

    Create Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,11 @@
+contract Ownable {
+    address internal owner;
+     modifier onlyOwner {
+    require(msg.sender == owner);
+    _; // run the function
+ }
+
+ constructor(){
+     owner = msg.sender;
+ }
+}
